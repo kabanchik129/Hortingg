3afcec1bfe4d1afd63d852ae00e942a36501b640
kabanchik129 <kaban88344@gmail.com>
2025-12-09 18:24:26 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,24 @@
-// firebase-config.js
-// Конфигурация Firebase (ЗАМЕНИТЕ НА СВОИ ДАННЫЕ!)
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyBIG0qp7QJt7S4dvwMHpuqsjyJ7kkyR64A",
     authDomain: "hortingg.firebaseapp.com",
     databaseURL: "https://hortingg-default-rtdb.firebaseio.com",
     projectId: "hortingg",
     storageBucket: "hortingg.firebasestorage.app",
     messagingSenderId: "374849935944",
-    appId: "1:374849935944:web:d288624d41b0cb07af167d",
-    measurementId: "G-3W9H6TT4NL"
-};
+    appId: "1:374849935944:web:6fb3266c2c3eb099af167d",
+    measurementId: "G-RK4SP2RZWE"
+  };
 
-// Экспортируем конфигурацию для использования в script.js
-window.firebaseConfig = firebaseConfig;
-
-console.log('Firebase конфигурация завантажена');
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
