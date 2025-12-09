f92db96bf05cf0a85d7ddcf9a629b7eeca35f498
kabanchik129 <kaban88344@gmail.com>
2025-12-09 18:11:03 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,7 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js
+// Конфигурация Firebase (ЗАМЕНИТЕ НА СВОИ ДАННЫЕ!)
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBIG0qp7QJt7S4dvwMHpuqsjyJ7kkyR64A",
     authDomain: "hortingg.firebaseapp.com",
     databaseURL: "https://hortingg-default-rtdb.firebaseio.com",
@@ -16,9 +10,9 @@
     messagingSenderId: "374849935944",
     appId: "1:374849935944:web:d288624d41b0cb07af167d",
     measurementId: "G-3W9H6TT4NL"
-  };
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Экспортируем конфигурацию для использования в script.js
+window.firebaseConfig = firebaseConfig;
+
+console.log('Firebase конфигурация завантажена');
