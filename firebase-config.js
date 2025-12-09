3eec2d18201277ed8bb6f788f4bd0445b399802e
kabanchik129 <kaban88344@gmail.com>
2025-12-09 18:27:07 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,24 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js - УПРОЩЕННАЯ ВЕРСИЯ
+// Если Firebase не нужен или не работает, можно использовать эту версию
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBIG0qp7QJt7S4dvwMHpuqsjyJ7kkyR64A",
-    authDomain: "hortingg.firebaseapp.com",
-    databaseURL: "https://hortingg-default-rtdb.firebaseio.com",
-    projectId: "hortingg",
-    storageBucket: "hortingg.firebasestorage.app",
-    messagingSenderId: "374849935944",
-    appId: "1:374849935944:web:6fb3266c2c3eb099af167d",
-    measurementId: "G-RK4SP2RZWE"
-  };
+// Проверяем, не определена ли уже конфигурация
+if (!window.firebaseConfig) {
+    console.log('Используем локальную конфигурацию Firebase');
+    
+    // Тестовая конфигурация (для разработки)
+    window.firebaseConfig = {
+ apiKey : "AIzaSyBIG0qp7QJt7S4dvwMHpuqsjyJ7kkyR64A" , 
+  authDomain : "hortingg.firebaseapp.com" , 
+  databaseURL : "https://hortingg-default-rtdb.firebaseio.com" , 
+  projectId : "hortingg" , 
+  storageBucket : "hortingg.firebasestorage.app" , 
+  messagingSenderId : "374849935944" , 
+  appId : "1:374849935944:web:6fb3266c2c3eb099af167d" , 
+  MeasurementId : "G-RK4SP2RZWE" 
+    };
+    
+    // Отключаем реальные запросы к Firebase
+    window.firebaseDisabled = true;
+}
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+console.log('Конфигурация Firebase завантажена');
