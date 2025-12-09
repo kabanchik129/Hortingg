14d26cf64f545661847ab641c30557d959ca64bf
kabanchik129 <kaban88344@gmail.com>
2025-12-09 22:52:24 +0100

    Change Supabase key in configuration
    Updated Supabase configuration with a new key.

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,7 @@ console.log('Завантаження конфігурації...');
 // Если не хотите использовать Supabase, просто оставьте это
 window.SUPABASE_CONFIG = {
     url: 'https://zgumvczkfaracjqvdjzm.supabase.co',  // Замените на реальный URL
-    key: 'sb_publishable_02yXlmq8Tdob9mCplLOcOg_amQO8c0k'                    // Замените на реальный ключ
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndW12Y3prZmFyYWNqcXZkanptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyODY0NjcsImV4cCI6MjA4MDg2MjQ2N30.H8yUfSceeXgrm75ra5ORlj9on8t80Y8APzH4k3OeF1o'                    // Замените на реальный ключ
 };
 
 // Инициализируем пустой клиент если Supabase не загрузился
