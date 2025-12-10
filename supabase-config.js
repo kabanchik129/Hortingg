324c8d51deef0a3b357ec977d5eff785356d2626
kabanchik129 <kaban88344@gmail.com>
2025-12-10 16:26:00 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -4,7 +4,7 @@ console.log('Завантаження конфігурації...');
 // Конфигурация Supabase (замени на свои реальные данные)
 window.SUPABASE_CONFIG = {
     url: 'https://zgumvczkfaracjqvdjzm.supabase.co',  // Замени на свой URL
-    key: 'sb_publishable_02yXlmq8Tdob9mCplLOcOg_amQO8c0k'  // Замени на свой ключ
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndW12Y3prZmFyYWNqcXZkanptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyODY0NjcsImV4cCI6MjA4MDg2MjQ2N30.H8yUfSceeXgrm75ra5ORlj9on8t80Y8APzH4k3OeF1o'  // Замени на свой ключ
 };
 
 // Если Supabase SDK не загружен, выводим предупреждение
