09fb99fca4fff1fbf4bcb192a4025588540af798
kabanchik129 <kaban88344@gmail.com>
2025-12-11 14:44:11 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-// supabase-config.js
-
-import { createClient } from '@supabase/supabase-js'
-
-// ⚠️ ВСТАВЬ СВОИ ДАННЫЕ
-const SUPABASE_URL = "https://zgumvczkfaracjqvdjzm.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndW12Y3prZmFyYWNqcXZkanptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyODY0NjcsImV4cCI6MjA4MDg2MjQ2N30.H8yUfSceeXgrm75ra5ORlj9on8t80Y8APzH4k3OeF1o"
-
-export const supabase = createClient(https://zgumvczkfaracjqvdjzm.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpndW12Y3prZmFyYWNqcXZkanptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyODY0NjcsImV4cCI6MjA4MDg)
